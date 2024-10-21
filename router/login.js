7e24130597ab38fad05a7d23ea16146b9ebbc589
anupam1702 <anupak144@gmail.com>
2024-10-21 23:22:48 +0530

    variable change

--- router/login.js
+++ router/login.js
@@ -20,6 +20,8 @@ router.post("/", async (req,res)=>{
         if (!user) {
             return res.status(404).json({ error: "User not found" });
         }
+        console.log("yo");
+        
 
         const isMatch= await bcrypt.compare(password,user.password || '');
        
